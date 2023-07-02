092a60b16b1958b4651dd7f4fa6f4774fb393232
nikhilgla <nikhil.baghel.32@gmail.com>
2023-07-02 19:09:16 +0530

    quert selector

--- demo.js
+++ demo.js
@@ -23,8 +23,20 @@ var li = document.getElementsByTagName('li')
 console.log(li)
 console.log(li[1])
 li[1].textContent = "kuch bhi li"
-li[2].style.background = "green"
+// li[2].style.background = "green"
 
 for(var i=0;i<li.length;i++){
 li[i].style.fontWeight = "bold";
+}
+
+var header = document.querySelector('#header-title')
+header.innerText = "By query selector"
+
+var secondItem = document.querySelector('.list-group-item:nth-child(3)')
+secondItem.innerText = "hahahahha"
+secondItem.style.background = "green"
+
+var odd = document.querySelectorAll('li:nth-child(odd)')
+for(var i=0;i<odd.length;i++){
+    odd[i].style.background = "pink"
 }
