51d951df3bbab0b2945ae5f25e629edb99453766
nikhilgla <nikhil.baghel.32@gmail.com>
2023-07-02 18:22:20 +0530

    borderbottom

--- demo.js
+++ demo.js
@@ -1,1 +1,10 @@
 console.log("hello");
+//console.dir(document);
+
+var headerTitle = document.getElementById('header-title')
+//console.log(headerTitle);
+
+headerTitle.innerText = "yoyoyo"
+
+var header = document.getElementById('main-header')
+header.style.borderBottom = "5px solid black"
