e0e499233e1e4c8667a07d73db7a7e4b1ae8d63f
nikhilgla <nikhil.baghel.32@gmail.com>
2023-07-02 18:45:39 +0530

    new li with tagname

--- demo.js
+++ demo.js
@@ -6,15 +6,25 @@ var headerTitle = document.getElementById('header-title')
 
 headerTitle.innerText = "yoyoyo"
 
-var header = document.getElementById('main-header')
-header.style.borderBottom = "5px solid black"
+// var header = document.getElementById('main-header')
+// header.style.borderBottom = "5px solid black"
 
-var items = document.getElementsByClassName('list-group-item')
-console.log(items)
-console.log(items[1])
-items[1].textContent = "kuch bhi"
-items[2].style.background = "green"
+// var items = document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[1])
+// items[1].textContent = "kuch bhi"
+// items[2].style.background = "green"
 
-for(var i=0;i<items.length;i++){
-items[i].style.fontWeight = "bold";
+// for(var i=0;i<items.length;i++){
+// items[i].style.fontWeight = "bold";
+// }
+
+var li = document.getElementsByTagName('li')
+console.log(li)
+console.log(li[1])
+li[1].textContent = "kuch bhi li"
+li[2].style.background = "green"
+
+for(var i=0;i<li.length;i++){
+li[i].style.fontWeight = "bold";
 }
