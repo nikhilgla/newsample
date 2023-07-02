9b3c3d36ce5338b79a09ab386bf8c4d2149fa440
nikhilgla <nikhil.baghel.32@gmail.com>
2023-07-02 18:35:44 +0530

    font bold

--- demo.js
+++ demo.js
@@ -8,3 +8,13 @@ headerTitle.innerText = "yoyoyo"
 
 var header = document.getElementById('main-header')
 header.style.borderBottom = "5px solid black"
+
+var items = document.getElementsByClassName('list-group-item')
+console.log(items)
+console.log(items[1])
+items[1].textContent = "kuch bhi"
+items[2].style.background = "green"
+
+for(var i=0;i<items.length;i++){
+items[i].style.fontWeight = "bold";
+}
