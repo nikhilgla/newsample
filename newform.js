04f04cbcad888cd9a30e8397c0c4c5114b98b51a
nikhilgla <nikhil.baghel.32@gmail.com>
2023-07-05 12:44:52 +0530

    store multiple users

--- newform.js
+++ newform.js
@@ -10,17 +10,24 @@ function onSubmit(e) {
   console.log(nameInput.value);
   localStorage.setItem(nameInput.value , emailInput.value);
   console.log(localStorage.getItem(nameInput.value));
+  
+  var itemList = document.querySelector('.items')
+  var li =document.createElement('li')
+  li.className="item";
+  li.appendChild(document.createTextNode(nameInput.value +"  "+ emailInput.value));
+
+  itemList.appendChild(li)
 }
 
-let myObj = {
-    name:"nikhil",
-    age:23
-};
-console.log(myObj);
-localStorage.setItem("nik1" , myObj);
-let myObj_forLS = JSON.stringify(myObj);
-console.log(myObj_forLS);
-localStorage.setItem("nik2" , myObj_forLS);
+// let myObj = {
+//     name:"nikhil",
+//     age:23
+// };
+// console.log(myObj);
+// localStorage.setItem("nik1" , myObj);
+// let myObj_forLS = JSON.stringify(myObj);
+// console.log(myObj_forLS);
+// localStorage.setItem("nik2" , myObj_forLS);
 
-let myObj_byLS = JSON.parse(localStorage.getItem('nik2'));
-console.log(myObj_byLS);
+// let myObj_byLS = JSON.parse(localStorage.getItem('nik2'));
+// console.log(myObj_byLS);
